1080ebfc187b3e60c87e44e398c91c5a3389e681
cangulerr <71367194+guler368@users.noreply.github.com>
2025-09-24 16:23:57 +0300

    Add files via upload

--- ConsoleApp.java
+++ ConsoleApp.java
@@ -1,8 +1,31 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 public class ConsoleApp {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the size of your array: ");
+        int size = in.nextInt();
+        int[] arr = arrayCreator(size);
+
+        System.out.println("1- Find the maximum and minimum");
+        System.out.println("2- Find the average");
+        System.out.println("3- Find the sum of odd-indexed numbers");
+        System.out.println("4- Find the sum of even-indexed numbers");
+        System.out.println("5- Exit");
+        System.out.println("Enter your choice");
+        int choice = in.nextInt();
+
+        while(choice != 5){
+            System.out.println("1- Find the maximum and minimum");
+            System.out.println("2- Find the average");
+            System.out.println("3- Find the sum of odd-indexed numbers");
+            System.out.println("4- Find the sum of even-indexed numbers");
+            System.out.println("5- Exit");
+            System.out.println("Enter your choice");
+            choice = in.nextInt();
+            
+        }
     }
     public static int[] arrayCreator( int arraySize ){
         Random joker = new Random();
